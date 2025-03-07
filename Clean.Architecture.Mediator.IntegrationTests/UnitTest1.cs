71105d742714c5b19fddd25a71022b0fb83cebf0
steven-repositories <stevenlestatan@gmail.com>
2025-03-08 05:10:05 +0800

    Added xUnit projects for unit and integration tests implementation

---
+++ Clean.Architecture.Mediator.IntegrationTests/UnitTest1.cs
@@ -0,0 +1,8 @@
+namespace Clean.Architecture.Mediator.IntegrationTests {
+    public class UnitTest1 {
+        [Fact]
+        public void Test1() {
+
+        }
+    }
+}
---
+++ Clean.Architecture.Mediator.UnitTests/UnitTest1.cs
@@ -0,0 +1,8 @@
+namespace Clean.Architecture.Mediator.UnitTests {
+    public class UnitTest1 {
+        [Fact]
+        public void Test1() {
+
+        }
+    }
+}
